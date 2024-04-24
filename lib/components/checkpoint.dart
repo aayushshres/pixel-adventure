751336a9888b8ef9953981517f66e558ab53c8e3
Aayush Shrestha <aayushshres20@gmail.com>
2024-04-25 03:50:45 +0530

    checkpoint animation added

--- lib/components/checkpoint.dart
+++ lib/components/checkpoint.dart
@@ -1,17 +1,25 @@
 import 'dart:async';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
+import 'package:pixel_adventure/components/player.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 class Checkpoint extends SpriteAnimationComponent
-    with HasGameRef<PixelAdventure> {
+    with HasGameRef<PixelAdventure>, CollisionCallbacks {
   Checkpoint({
     super.position,
     super.size,
   });
 
+  bool reachedCheckpoint = false;
   @override
   FutureOr<void> onLoad() {
+    add(RectangleHitbox(
+      position: Vector2(18, 56),
+      size: Vector2(12, 8),
+      collisionType: CollisionType.passive,
+    ));
     animation = SpriteAnimation.fromFrameData(
       game.images
           .fromCache("Items/Checkpoints/Checkpoint/Checkpoint (No Flag).png"),
@@ -22,5 +30,37 @@ class Checkpoint extends SpriteAnimationComponent
       ),
     );
     return super.onLoad();
+  }
+
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    if (other is Player && !reachedCheckpoint) _reachedCheckpoint();
+    super.onCollision(intersectionPoints, other);
+  }
+
+  void _reachedCheckpoint() {
+    reachedCheckpoint = true;
+    animation = SpriteAnimation.fromFrameData(
+      game.images.fromCache(
+          "Items/Checkpoints/Checkpoint/Checkpoint (Flag Out) (64x64).png"),
+      SpriteAnimationData.sequenced(
+        amount: 26,
+        stepTime: 0.05,
+        textureSize: Vector2.all(64),
+        loop: false,
+      ),
+    );
+    const flagDuration = Duration(microseconds: 1300);
+    Future.delayed(flagDuration, () {
+      animation = SpriteAnimation.fromFrameData(
+        game.images.fromCache(
+            "Items/Checkpoints/Checkpoint/Checkpoint (Flag Idle)(64x64).png"),
+        SpriteAnimationData.sequenced(
+          amount: 10,
+          stepTime: 0.05,
+          textureSize: Vector2.all(64),
+        ),
+      );
+    });
   }
 }
